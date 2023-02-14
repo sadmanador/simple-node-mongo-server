fb27bd05ef70a9985e83780a6accbb9fb1e80a63
sadmanador <sadman.ador@gmail.com>
2023-02-15 01:17:33 +0600

    64-2 done!

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
-app.get('/', (req, res) => {
-res.send('Hello world')
+app.use(cors());
+
+const person = [
+  {id:1, name: "Tasnuva", email: "tas@gmail.com" },
+  {id:2, name: "esha", email: "esh@gmail.com" },
+  {id:3, name: "farhana", email: "far@gmail.com" },
+];
+
+app.get("/", (req, res) => {
+  res.send("user server is running");
+});
+
+app.get('/users', (req, res) => {
+res.send(person)
 });
 
 app.listen(port, () => {
-console.log(`listening port 5000`);
+  console.log(`listening port 5000`);
 });
