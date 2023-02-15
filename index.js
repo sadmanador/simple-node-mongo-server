2370edc447388729cc2eecdcab7af95d80c0f470
sadmanador <sadman.ador@gmail.com>
2023-02-15 15:27:39 +0600

    64-3 working

--- index.js
+++ index.js
@@ -3,20 +3,31 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
+//middleware
 app.use(cors());
+app.use(express.json());
 
-const person = [
-  {id:1, name: "Tasnuva", email: "tas@gmail.com" },
-  {id:2, name: "esha", email: "esh@gmail.com" },
-  {id:3, name: "farhana", email: "far@gmail.com" },
+const users = [
+  { id: 1, name: "Tasnuva", email: "tas@gmail.com" },
+  { id: 2, name: "esha", email: "esh@gmail.com" },
+  { id: 3, name: "farhana", email: "far@gmail.com" },
 ];
 
 app.get("/", (req, res) => {
   res.send("user server is running");
 });
 
-app.get('/users', (req, res) => {
-res.send(person)
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api called");
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  console.log(user);
+  res.send(user);
 });
 
 app.listen(port, () => {
