468b126e0160f0fb8cf7a274d39ddd363ad235ac
sadmanador <sadman.ador@gmail.com>
2023-02-15 19:47:04 +0600

    64-6 done mongoDb conneted to node

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//server credential
+//user: sadmanAdor
+//password: 35BFKXAJcX24x8P9
 
 const users = [
   { id: 1, name: "Tasnuva", email: "tas@gmail.com" },
   { id: 2, name: "esha", email: "esh@gmail.com" },
   { id: 3, name: "farhana", email: "far@gmail.com" },
 ];
+
+
+
+const uri = "mongodb+srv://sadmanAdor:35BFKXAJcX24x8P9@cluster0.9mathic.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  console.log("database connected")
+  const collection = client.db("simple-node-mongo-sever").collection("users");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get("/", (req, res) => {
   res.send("user server is running");
